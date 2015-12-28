32695a3cddb003c8785b1847ced12a85e7ff39ca
Jake <jdt97@scarletmail.rutgers.edu>
2015-12-27 19:04:27 -0500

    Split classes into two separate files.

---
+++ CurrencyOption.py
@@ -0,0 +1,226 @@
+from math import exp, log, sqrt, pi
+from multiprocessing import Process, Pipe, Manager
+
+
+class CurrencyOption:
+	twoPi = 2*pi
+
+	def __init__(self, name, buy, sell, expireTime, indicative, pipe, index):
+		global riskFreeRates, exchangeRates
+		self.name = name
+		self.buyPrice = buy
+		self.sellPrice = sell
+		self.expiry = expireTime
+		if isinstance(indicative, float):
+			self.underlying = indicative
+			doExp = True
+		else:
+			self.underlying = exchangeRates[name.split(" ")[0]]
+			doExp = False
+		Process(target=self.updateFields, args=(pipe, index, doExp)).start()
+		self.strike = float(name.split(" ")[-2].replace(">", ""))
+		self.countries = [c for c in name.split(" ")[0].split("/")]
+		self.conversionFactor = 1/self.underlying
+		self.r_domestic = riskFreeRates[self.countries[0]]
+		self.r_foreign = riskFreeRates[self.countries[1]]
+		self.d1 = 0
+		self.volatility = self.calculateVolatility(0.05)
+		self.d2 = self.d1 - self.volatility*sqrt(self.expiry)
+
+	def updateFields(self, pipe, index, doExpiry):
+		"""Updates the buy, sell, expire time, and underlying value for the option."""
+		
+		while doExpiry:
+			self.sellPrice = pipe.recv()[0][-1]
+			self.buyPrice = pipe.recv()[1][-1]
+			self.expiry = pipe.recv()[2]
+			self.underlying = pipe.recv()[3]
+		
+		while not doExpiry:
+			self.buyPrice = motherOfAllBuyPrices[index][-1]
+			self.sellPrice = motherOfAllSellPrices[index][-1]
+			self.underlying = motherOfAllUnderlying[index]
+
+	def convertUnits(self):
+		"""Returns the correct conversion factor, aka exchange rate, for the given currencies.
+		This seems useless. <- I wrote that year ago, why?
+		Probably because it's not used anywhere.
+		No, it's because all it really does is return a reciprocal of a value."""
+
+		return 1.0/self.underlying
+
+	def buy(self, lotSize=1, short=False):
+		"""Buys the option."""
+		
+		global ticketsOpen, purchaseInProgress
+
+		while purchaseInProgress:
+			pass
+		purchaseInProgress = True
+		try:
+			driver.find_element_by_link_text(self.name).click()
+			ticketsOpen += 1
+			myTicket = ticketsOpen
+		except:
+			return "Link_text not found."
+
+		#This is here to prevent the code from executing before the order form has even opened.
+		loaded = False
+		while not loaded:
+			try:
+				float(driver.execute_script("return window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('dmaPriceCurrent').textContent"))
+				loaded = True
+			except:
+				pass
+
+		#This chunk actually places the order.
+		#The above chunk of code doesn't slow the execution enough.
+		#Unfortunately I cannot come up with a better way of further slowing it down enough other than time.sleep().
+		time.sleep(0.45)
+		try:
+			driver.execute_script("window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('directionChange').click()") #Once for buy, twice for sell.
+			driver.execute_script("window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('size').value = "+str(lotSize))
+			if short:
+				driver.execute_script("window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('directionChange').click()")
+				driver.execute_script("window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('level').value = "+str(self.sellPrice))
+			else:
+				driver.execute_script("window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('level').value = "+str(self.buyPrice))
+			driver.execute_script("window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('btnSubmit').click()")
+			loaded = False
+		except:
+			print("Error filling buyslip.")
+
+		while not loaded:
+			try:
+				driver.execute_script("window.parent.frames['ifrBetslip-"+str(myTicket)+"'].document.getElementById('betslipBtnClose').click()")
+				loaded = True
+			except:
+				pass
+		
+		ticketsOpen -= 1
+		purchaseInProgress = False
+
+	
+	def calculateVolatility(self, precision):
+		"""Calculates volatility using the bisection method on the Garman-Kohlhagen model.
+		This function is also responsible for calculating d1 and d2.
+		Unit problems have mostly been ironed out, but there may be some left."""
+		
+		low = 0.01
+		high = 300.0
+		
+		while True:
+			self.volatility = (high + low)/2.0
+			
+			self.d1 = (log(self.underlying/self.strike) + (self.r_domestic - self.r_foreign + 0.5*self.volatility*self.volatility)*self.expiry)/(self.volatility*sqrt(self.expiry))
+			value = exp(-self.r_domestic*self.expiry)*norm.cdf(self.d1, 0, 1)*self.buyPrice*self.conversionFactor
+			
+			if (abs(self.buyPrice - value) <= precision)  or (high <= 0.1) or (low >= 299.9):
+				break
+			elif value < self.buyPrice:
+				low = self.volatility
+			elif value > self.buyPrice:
+				high = self.volatility
+		
+		self.d2 = self.d1 - self.volatility*sqrt(self.expiry)		
+		return self.volatility
+	
+#	"""	 GREEKS	     """
+# Note that many of these functions have a*a in them, since it is slightly faster than a**2.
+
+	def printGreeks(self):
+		"""Simply calculates all of the major Greeks and prints them out.
+		Used for debugging purposes."""
+		
+		print("d1 =\t\t\t", self.d1)
+		print("d2 =\t\t\t", self.d2)
+		print("Volatility =\t", self.volatility)
+		print("Delta (call) =\t", self.delta(short=False))
+		print("Delta (put) =\t", self.delta(short=True))
+		print("Leverage (call) =\t", self.leverage(short=False))
+		print("Leverage (put) =\t", self.leverage(short=True))
+		print("Theta (call)=\t", self.theta(short=False))
+		print("Theta (put)=\t", self.theta(short=True))
+		print("Vega =\t\t\t", self.vega())
+		print("Rho (call)=\t\t", self.rho(short=True))
+		print("Rho (put)=\t\t", self.rho(short=False))
+		print("Gamma =\t\t\t", self.gamma())
+		print("Vanna =\t\t\t", self.vanna())
+		print("Vomma =\t\t\t", self.vomma())
+		print("Speed =\t\t\t", self.speed())
+		print("Zomma =\t\t\t", self.zomma())
+		print("Ultima =\t\t", self.ultima())
+
+	def delta(self, short=False):
+		"""Derivative of the value with respect to the underlying."""
+		
+		if short:
+			return -exp(-self.r_foreign * self.expiry) * norm.cdf(-self.d1)
+		else:
+			return exp(-self.r_foreign * self.expiry) * norm.cdf(self.d1)
+	
+	def leverage(self, short=False):
+		"""Delta * underlying/price."""
+		
+		if short:
+			return -exp(-self.r_foreign * self.expiry) * norm.cdf(-self.d1) * (self.underlying/self.strike)
+		else:
+			return exp(-self.r_foreign * self.expiry) * norm.cdf(self.d1) * (self.underlying/self.strike)
+	
+	def theta(self, short=False):
+		"""Minus one times the derivative of value with respect to time."""
+		
+		if short:
+			return (-exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2) * self.underlying * self.volatility/(2*sqrt(twoPi*self.expiry)))-(self.r_foreign*exp(-self.r_foreign*self.expiry)*norm.cdf(-self.d1))+(self.r_domestic*exp(self.r_domestic*self.expiry)*self.strike*norm.cdf(-self.d2))
+		else:
+			return (-exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2) * self.underlying * self.volatility/(2*sqrt(twoPi*self.expiry)))+(self.r_foreign*exp(-self.r_foreign*self.expiry)*norm.cdf(self.d1))-(self.r_domestic*exp(self.r_domestic*self.expiry)*self.strike*norm.cdf(self.d2))
+	
+	def vega(self):
+		"""Derivative of value with respect to volatility."""
+		
+		return exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2)*self.underlying*sqrt(self.expiry/(twoPi))
+	
+	def rho(self, short=False):
+		"""Derivative of value with respect to the interest rate."""
+		
+		if short:
+			return -self.expiry * exp(-self.r_domestic*self.expiry) * self.strike * norm.cdf(-self.d2)
+		else:
+			return self.expiry * exp(-self.r_domestic*self.expiry) * self.strike * norm.cdf(self.d2)
+	
+	def gamma(self):
+		"""Derivative of delta with respect to underlying.
+		Second Derivative of with value respect to underlying."""
+		
+		return exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2)/(self.underlying * self.volatility * sqrt(twoPi*self.expiry))
+	
+	def vanna(self):
+		"""Derivative of delta with respect to volatility.
+		Derivative of vega with respect to underlying.
+		Second Derivative of value with respect once to underlying and volatility."""
+		
+		return -exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2) * self.d2 /(self.volatility * sqrt(twoPi))
+	
+	def vomma(self):
+		"""Derivative of vega with respect to volatility.
+		Second Derivative of value with respect to volatility."""
+		
+		return self.underlying * exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2) * sqrt(self.expiry) * self.d1 * self.d2 / self.volatility
+	
+	def speed(self):
+		"""Derivative of gamma with respect to underlying.
+		Third Derivative of value with respect to underlying."""
+		
+		return -((exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2)/(self.underlying * self.volatility * sqrt(twoPi*self.expiry)))/self.underlying) * (1 + self.d1/(self.volatility*sqrt(self.expiry)))
+	
+	def zomma(self):
+		"""Derivative of gamma with respect to volatility.
+		Third Derivative of value with respect twice to underlying and once to volatility."""
+		
+		return (exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2)/(self.underlying * self.volatility * sqrt(twoPi*self.expiry))) * ((self.d1*self.d2 -1)/self.volatility)
+	
+	def ultima(self):
+		"""Derivative of vomma with respect to volatility.
+		Third Derivative of value with respect to volatility."""
+		
+		return ((-exp(-self.r_foreign * self.expiry - (self.d1*self.d1)/2)*self.underlying*sqrt(self.expiry/twoPi))/(self.volatility*self.volatility)) * (self.d1*self.d2*(1-self.d1*self.d2) + self.d1*self.d1 + self.d2*self.d2)
